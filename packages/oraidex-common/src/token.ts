e43ccbb3131893c70efe720601394b3d443ec528
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-12-01 15:12:18 +0700

    feat: bump version to 2.0.0-beta.2 and enhance getOraidexCommonAttribute with additional keys

--- packages/oraidex-common/src/token.ts
+++ packages/oraidex-common/src/token.ts
@@ -38,41 +38,41 @@ export interface FormatNumberDecimal {
   unitPosition?: "prefix" | "suffix";
 }
 
-const evmDenomsMap = {
+export const evmDenomsMap = {
   kwt: [KWTBSC_ORAICHAIN_DENOM],
   milky: [MILKYBSC_ORAICHAIN_DENOM],
   injective: [INJECTIVE_ORAICHAIN_DENOM]
 };
-const minAmountSwapMap = {
+export const minAmountSwapMap = {
   trx: 10
 };
 
-export const getTokensFromNetwork = (network: CustomChainInfo): TokenItemType[] => {
-  return network.currencies.map((currency) => ({
-    name: currency.coinDenom,
-    org: network.chainName,
-    coinType: network.bip44.coinType,
-    contractAddress: currency.contractAddress,
-    prefix: currency?.prefixToken ?? network.bech32Config?.bech32PrefixAccAddr,
-    coinGeckoId: currency.coinGeckoId,
-    denom: currency.coinMinimalDenom,
-    bridgeNetworkIdentifier: currency.bridgeNetworkIdentifier,
-    decimals: currency.coinDecimals,
-    bridgeTo: currency.bridgeTo,
-    chainId: network.chainId,
-    rpc: network.rpc,
-    lcd: network.rest,
-    cosmosBased: network.networkType === "cosmos",
-    maxGas: (network.feeCurrencies?.[0].gasPriceStep?.high ?? 0) * 20000,
-    gasPriceStep: currency.gasPriceStep,
-    feeCurrencies: network.feeCurrencies,
-    minAmountSwap: minAmountSwapMap[currency.coinMinimalDenom],
-    evmDenoms: evmDenomsMap[currency.coinMinimalDenom],
-    Icon: currency.coinImageUrl,
-    IconLight: currency.coinImageUrl,
-    icon: currency.coinImageUrl
-  })) as any;
-};
+// export const getTokensFromNetwork = (network: CustomChainInfo): TokenItemType[] => {
+//   return network.currencies.map((currency) => ({
+//     name: currency.coinDenom,
+//     org: network.chainName,
+//     coinType: network.bip44.coinType,
+//     contractAddress: currency.contractAddress,
+//     prefix: currency?.prefixToken ?? network.bech32Config?.bech32PrefixAccAddr,
+//     coinGeckoId: currency.coinGeckoId,
+//     denom: currency.coinMinimalDenom,
+//     bridgeNetworkIdentifier: currency.bridgeNetworkIdentifier,
+//     decimals: currency.coinDecimals,
+//     bridgeTo: currency.bridgeTo,
+//     chainId: network.chainId,
+//     rpc: network.rpc,
+//     lcd: network.rest,
+//     cosmosBased: network.networkType === "cosmos",
+//     maxGas: (network.feeCurrencies?.[0].gasPriceStep?.high ?? 0) * 20000,
+//     gasPriceStep: currency.gasPriceStep,
+//     feeCurrencies: network.feeCurrencies,
+//     minAmountSwap: minAmountSwapMap[currency.coinMinimalDenom],
+//     evmDenoms: evmDenomsMap[currency.coinMinimalDenom],
+//     Icon: currency.coinImageUrl,
+//     IconLight: currency.coinImageUrl,
+//     icon: currency.coinImageUrl
+//   })) as any;
+// };
 
 // let oraiCommon: OraiCommon = null;
 // let tokenConfig: {
